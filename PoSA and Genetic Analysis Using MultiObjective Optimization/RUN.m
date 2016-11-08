5f3781c0775c1e1866f761bf7d49679d36c4256b
ssahebkashaf <sarasahebkashaf@Saras-MacBook-Air.local>
2016-11-08 18:32:57 -0500

    Remove duplicated directory

--- PoSA and Genetic Analysis Using MultiObjective Optimization/RUN.m
+++
@@ -1,8 +0,0 @@
-
-
-warning('off', 'all')
-
-num_cores = 4;
-%addAttachedFiles(gcp,{'glpk.m','glpkcc'});
-expFBA(140,6000,'iCdiff833',4995,num_cores);
-
--- PoSA and Genetic Analysis Using MultiObjective Optimization/read_me.txt
+++
@@ -1,9 +0,0 @@
-run SA_main to evaluate PoSA for Rhodobacter_CO2.mat
-
-default:
-
-n_val=10;
-K=4;
-KK=10;
-n_pathway=45
-
